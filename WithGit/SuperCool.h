79eb1f3474c5b6fa5211dd237747aa8bd797da24
bt <bernardotolosajr@yahoo.com>
2013-03-09 11:22:54 +0800

    new features

---
+++ WithGit/SuperCool.h
@@ -0,0 +1,13 @@
+//
+//  SuperCool.h
+//  WithGit
+//
+//  Created by NardVader on 3/9/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuperCool : UIViewController
+
+@end
