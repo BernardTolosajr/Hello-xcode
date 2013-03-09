0fb66d13c075eae856375b0d3700028b06aa0232
bt <bernardotolosajr@yahoo.com>
2013-03-09 12:48:32 +0800

    add new class

---
+++ WithGit/again.h
@@ -0,0 +1,13 @@
+//
+//  again.h
+//  WithGit
+//
+//  Created by NardVader on 3/9/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface again : NSObject
+
+@end
---
+++ WithGit/again.m
@@ -0,0 +1,13 @@
+//
+//  again.m
+//  WithGit
+//
+//  Created by NardVader on 3/9/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "again.h"
+
+@implementation again
+
+@end
